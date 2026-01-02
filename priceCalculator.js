3515e9c0ee08f3e75f0010d209f7e10aa81e899b
Vishalini R <vishalinis287@gmail.com>
2026-01-02 09:20:07 +0530

    Add price calculator for single and multiple products

---
+++ priceCalculator.js
@@ -0,0 +1,20 @@
+//adding single product price
+var product="Shirt"
+var price=1500
+var Tax=180
+console.log("Product Name: "+product)
+console.log("Tax: "+Tax)
+console.log("Product price: "+price)
+var total=price+Tax
+console.log("Total cost: "+total)
+
+//multiple product price
+
+var Fname="Pineapple"
+var count=7
+var price=20
+var total=count * price
+console.log("FruitName: "+Fname)
+console.log("FruitCount: "+count)
+console.log("FruitPrice: "+price)
+console.log("Total Amount: "+total)
