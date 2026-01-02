20fad3716835503ea2d755d3965916c7dbe6ffe8
Vishalini R <vishalinis287@gmail.com>
2026-01-02 09:38:27 +0530

    Add favourite function to log personal favorites

---
+++ favouriteFunction.js
@@ -0,0 +1,12 @@
+var factors="Surya,Nani"
+var fplayers="Dhoni,Virat,Rohit"
+var fmovie="Hi Nanna"
+
+function favourite()
+{
+    console.log("Favourite actors:" +factors)
+    console.log("Favourite players:" +fplayers)
+    console.log("Favourite movie:" +fmovie)
+}
+
+favourite()
